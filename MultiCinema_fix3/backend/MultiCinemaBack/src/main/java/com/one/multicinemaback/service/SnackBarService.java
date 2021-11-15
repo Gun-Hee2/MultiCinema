f864496c7855a6fe20674937d98495679ae5db66
Gun-Hee2 <alg75@naver.com>
2021-11-15 23:58:53 +0900

    Update Fix3 Code

---
+++ MultiCinema_fix3/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/SnackBarService.java
@@ -0,0 +1,27 @@
+package com.one.multicinemaback.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.one.multicinemaback.dao.SnackBarDao;
+import com.one.multicinemaback.dto.SnackBarDto;
+
+@Service
+@Transactional
+public class SnackBarService {
+	
+	@Autowired
+	SnackBarDao dao;
+	
+	public List<SnackBarDto> snackbarlist(String category) {
+		return dao.snackbarlist(category);
+	}
+	
+	public SnackBarDto snackbardetail(String product) {
+		return dao.snackbardetail(product);
+	}
+
+}
