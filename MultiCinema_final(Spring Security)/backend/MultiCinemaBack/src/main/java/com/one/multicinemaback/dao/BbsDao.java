402afd433aeb67fe3b280e8ca81623c19ecb99c9
Gunhee Heo <alg75@naver.com>
2022-03-14 14:18:44 +0900

    Update BbsDao.java

--- MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/dao/BbsDao.java
+++ MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/dao/BbsDao.java
@@ -13,23 +13,33 @@ import com.one.multicinemaback.dto.BbsParam;
 @Repository
 public interface BbsDao extends Serializable {
 	
+	// 고객센터 글 목록 불러오기
 	List<BbsDto> getbbslist(BbsParam param);
 	
+	// 글의 총수 카운트
 	int getbbscount(BbsParam param);
 	
+	// 새 글 작성
 	boolean writeBbs(BbsDto bbs);
 	
+	// 글 세부내용 불러오기
 	BbsDto getBbs(int seq);
 	
+	// 글 조회수 불러오기
 	void readcount(int seq);
 	
+	// 답글 글 순서 최신화
 	boolean answerup(BbsDto dto);
 	
+	// 답글 작성
 	boolean answerin(BbsDto dto);
 	
+	// 글 수정하기
 	boolean updateBbs(BbsDto dto);
 	
+	// 글 삭제하기
 	boolean deleteBbs(BbsDto dto);
 	
+	// 내가 작성한 글 
 	List<BbsDto> mybbslist(String id);
 }
