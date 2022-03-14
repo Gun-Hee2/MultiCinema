e4aa7e29da5d677767e0040b3172d2bc43f5358d
Gunhee Heo <alg75@naver.com>
2022-03-14 19:34:35 +0900

    Update TicketService.java

--- MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/TicketService.java
+++ MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/TicketService.java
@@ -16,27 +16,28 @@ public class TicketService {
 	@Autowired
 	TicketDao dao;
 	
+	// 영화 티켓 예약리스트 불러오기(마이 페이지)
 	public List<TicketDto> getTodayList(String id){
 		return dao.getTodayList(id);
 	}
 	
+	// 영화 티켓 과거 예약리스트 불러오기(마이 페이지)
 	public List<TicketDto> getPastList(String id){
 		return dao.getPastList(id);
 	}
 	
+	// 영화  좌석 불러오기(티켓 예약 페이지)
 	public List<TicketDto> getSeatList(int msnum){
 		return dao.getSeatList(msnum);
 	}
 	
+	// 영화 티켓 카카오결제 내역 데이터 저장
 	public void inserttipay(TicketDto dto) {
 		dao.inserttipay(dto);
 	}
 	
+	// 영화 티켓 카카오결제 내역 데이터 불러오기
 	public TicketDto gettipay(TicketDto dto) {
 		return dao.gettipay(dto);
-	}
-	
-	public boolean deltipay(TicketDto dto) {
-		return dao.deltipay(dto);
 	}
 }
