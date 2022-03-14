4482e3eb4dabec3466e11344a05f60722fb1ff29
Gunhee Heo <alg75@naver.com>
2022-03-14 19:29:44 +0900

    Update TicketDao.java

--- MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/dao/TicketDao.java
+++ MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/dao/TicketDao.java
@@ -10,12 +10,19 @@ import com.one.multicinemaback.dto.TicketDto;
 @Mapper
 @Repository
 public interface TicketDao {
-
+        // 영화 티켓 예약리스트 불러오기
 	List<TicketDto> getTodayList(String id);
+	
+	// 영화 티켓 과거 예약리스트 불러오기
 	List<TicketDto> getPastList(String id);
+	
+	// 영화  좌석 불러오기
 	List<TicketDto> getSeatList(int msnum);
 	
+	// 영화 티켓 카카오결제 내역 데이터 저장
 	void inserttipay(TicketDto dto);
+	
+	// 영화 티켓 카카오결제 내역 데이터 불러오기
 	TicketDto gettipay(TicketDto dto);
-	boolean deltipay(TicketDto dto);
+	
 }
