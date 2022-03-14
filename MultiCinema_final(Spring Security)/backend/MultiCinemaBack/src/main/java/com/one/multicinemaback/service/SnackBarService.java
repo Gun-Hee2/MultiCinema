3968c8d7c17eeee065c7570f2d8237d0c1081b2d
Gunhee Heo <alg75@naver.com>
2022-03-14 15:39:14 +0900

    Update SnackBarService.java

--- MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/SnackBarService.java
+++ MultiCinema_final(Spring Security)/backend/MultiCinemaBack/src/main/java/com/one/multicinemaback/service/SnackBarService.java
@@ -16,14 +16,17 @@ public class SnackBarService {
 	@Autowired
 	SnackBarDao dao;
 	
+	//스낵바 메뉴 추가
 	public void addSnackBar(SnackBarDto product) {
 		dao.addSnackBar(product);
 	}
 	
+	//스낵바 리스트
 	public List<SnackBarDto> snackbarlist(String category) {
 		return dao.snackbarlist(category);
 	}
 	
+	//스낵바 상품 상세정보
 	public SnackBarDto snackbardetail(String product) {
 		return dao.snackbardetail(product);
 	}
